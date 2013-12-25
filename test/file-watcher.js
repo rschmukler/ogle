e7d543548c3bc825963480f7e3a3f4acd818a221
Ryan Schmukler <rschmukler@gmail.com>
2013-12-25 15:23:41 -0600

    Implemented file and directory watchers

--- test/file-watcher.js
+++ test/file-watcher.js
@@ -4,22 +4,58 @@ var expect = require('expect.js'),
 var FileWatcher = require('../lib/file-watcher.js');
 
 describe("File Watcher", function() {
-  var watcher;
+  var watcher,
+      testFile = __dirname + '/fixtures/test.txt',
+      testDir = __dirname + '/fixtures/testDir/';
 
-  before(function() {
-    watcher = new FileWatcher(__dirname + '/fixtures/test.txt');
-  });
+
 
   it('throws an error if file doesnt exist', function() {
-    try {
+    var makeWatcher = function() {
       watcher = new FileWatcher('./fixtures/not-real.txt');
-      throw new Error("Should not reach here");
-    } catch(e) {
-      expect(e.message).to.be("Attempted to watch a file that doesn't exist");
-    }
+    };
+    expect(makeWatcher).to.throwError("Attempted to watch a file that doesn't exist");
   });
 
-  it("emits changed events");
-  it("emits renamed events");
-  it("reattaches listeners on rename");
+  describe("files", function() {
+    beforeEach(function() {
+      watcher = new FileWatcher(testFile);
+    });
+
+    it("doesn't set itself as a directory", function() {
+      expect(watcher._directory).to.be(false);
+    });
+
+    it("emits 'change' events", function(done) {
+      watcher.once('change', function(path) {
+        expect(path).to.be(testFile);
+        done();
+      });
+      fs.writeFile(testFile, 'wee');
+    });
+
+    it("emits 'remove' events", function(done) {
+      watcher.once('remove', function(path) {
+        expect(path).to.be(testFile);
+        fs.writeFile(testFile, 'wee', done);
+      });
+      fs.unlink(testFile, 'wee');
+    });
+  });
+
+  describe("directories", function() {
+
+    beforeEach(function() {
+      watcher = new FileWatcher(testDir);
+    });
+
+    it("emits 'add' events", function(done) {
+      watcher.once('add', function(dir, newFile) {
+        expect(dir).to.be(testDir);
+        expect(newFile).to.be(testDir + 'newFile.txt');
+        fs.unlink(testDir + 'newFile.txt', done);
+      });
+      fs.writeFile(testDir + 'newFile.txt');
+    });
+  });
 });
